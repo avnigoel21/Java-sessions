8674d747ae926ab24ec683b1861c6f4663879091
Avni Goel <avnigoel98@gmail.com>
2023-11-29 00:20:54 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,170 @@
+class hello{
+    public static void main(String[] args){
+        // int num = 0;
+        // while(true){
+        //     if(num > 5) System.out.println(("hi"));
+
+        // }
+
+        // int b = 1;
+        // do{
+        //     System.out.println((b));
+        //     b++;
+        // }while(b<5);
+
+
+        // for(int i = 0; i < 50; i++){
+        //     if(i == 2){
+        //         System.out.println("Ending of loop");
+        //         continue;
+        //     }
+        //     System.out.println((i));
+        // }
+
+
+        // array - collection of similar types of data
+
+        // int [] marks = new int[6];
+        
+        // marks[0] = 100;
+        // marks[1] = 45;
+        // marks[2] = 100;
+        // marks[3] = 45;
+        // marks[4] = 100;
+        // System.out.println(marks[5]);
+
+        // int [] marks;  // declaration
+        // marks = new int[6]; //memeory allocation 
+
+
+        // int [] marks = {100, 45, 78, 34, 23 ,89};
+        // String[] students = new String[3];
+        // System.out.println(students[2]);
+
+
+        // int [] marks = {100, 45, 78, 34, 23 ,89};
+
+        // for(int i = 0; i < marks.length; i++ ){
+        //     System.out.println(marks[i]);
+        // }
+
+        int [] marks ; //1-D Array
+        int [][] flats; // 2-D Array
+
+        marks = new int[3];
+        flats = new int[2][3];
+
+        flats[0][0] = 101;
+        flats[0][1] = 102;
+        flats[0][2] = 103;
+        flats[1][0] = 201;
+        flats[1][1] = 202;
+        flats[1][2] = 203;
+
+        for(int i = 0 ; i < flats.length; i++){
+            for(int j = 0; j < flats[i].length ; j++){
+                System.out.print(flats[i][j]);
+                System.out.print((" "));
+            }
+            System.out.println("");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+        // System.out.println("Hello");
+        // System.out.println("Hello");
+        // System.out.print("Hello");
+        // System.out.print("hello");
+
+        // long a = 563445678345678L;
+        // float d = 45.85678F;
+        // double e = 45.9d;
+        // char c = 'A';
+        // int b;
+
+        // //int 23num = 90;
+        // int num23 = 56;
+
+        // int num = 34;
+        // int Num = 45;
+
+        // int num_of_2$numbers = 45;
+
+
+        // int number = 4;
+        // number += 2 ; 
+        // number -= 4;
+        // System.out.println(number);
+        
+
+        // if(3 != 5 && 2==2){
+        //     System.out.println("hello world");
+        // }
+        // else if (number == 2){
+        //     System.out.println("hello");
+        // }
+        // else{
+        //     System.out.println("hi");
+        // }
+
+
+
+    }
+}
+
+// IN-CODE DOCUMENTATION
+// FUNCTIONS & CLASSES
+
+// classes - PascalConvention
+// functions - camelCaseConvention
+
+// Data types - Primitive data types , Non Primitive Data types
+//primitive - 8 types ( byte, int, long , float, double, char, short, bool )
+
+
+// write a java prog which asks the user to enter his/her name and greets them with 
+//"hello <name> , have a nice day"
+
+
+//types of operators
+// 1) Airthmetic operators + , - , *, / , % , ++ ,--
+// 2) Assignment operators =, += , -= ,*=
+// 3) Comparison operators == , !=, >=, <=, > , <
+// 4) logical operators &  , ||, !
+//5) Bitwise operators & , | (operators bitwise)
+
+
+// loops : while , for, do-while
+
+
+// while(boolean condition){
+//     statement
+// }
+
+
+
+
+
+
+
+
+
+
+
+
