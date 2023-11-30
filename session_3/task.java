cc1cb7ab5d34bf66181f3756cd8cd9c9fa08d936
Avni Goel <avnigoel98@gmail.com>
2023-12-01 00:15:11 +0530

    added tasks

---
+++ session_3/task.java
@@ -0,0 +1,9 @@
+package session_3;
+
+public class task {
+    
+}
+
+
+// Write a function to find average of a set of numbers passed as arguments
+// Write a function to covert Celsius Temperature into Fahrenheit
