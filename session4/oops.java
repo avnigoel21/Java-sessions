34b7120d724a697d39014a9243677f8cdf86fe08
Avni Goel <avnigoel98@gmail.com>
2023-12-22 00:16:09 +0530

    working on recursion, oops

---
+++ session4/oops.java
@@ -0,0 +1,28 @@
+// package session4;
+
+// public class oops {
+
+//     public class Employee {
+//         int id;
+//         String name;
+//     }
+
+
+//     public static void main(String[] args) {
+//         System.out.println("this is our custom class");
+//         Employee harry = new Employee();
+//         harry.id = 12;
+//         harry.name = "HarryGarg";
+//         System.out.println(harry.id);
+//         System.out.println(harry.name);
+//     }
+// }
+
+
+
+// // exam application -> filled by the student -> application of that student
+// // class            -> object Instantiation  -> Object 
+
+// // class -> Employee
+// // attributes -> name, age, salary
+// // methods -> getSalary() , increment()
---
+++ session4/recursion.java
@@ -0,0 +1,37 @@
+package session4;
+
+public class recursion {
+    // function calling itself is recursion
+    static int factorial_recursion(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        else{
+            return n * factorial_recursion(n-1);
+        }
+    }
+
+    static int factorial_with_loops(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        else{
+            int product = 1;
+            for(int i = 1; i <= n ; i++){
+                product *= i;
+            }
+            return product;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial_recursion(5));
+        System.out.println(factorial_with_loops(5));
+    }
+}
+
+
+// factorial(1)  = 1
+// factorial(0) = 1
+// factorial(5) = 5 * 4 * 3 * 2 * 1
+// factorial(n) = n * n-1  * ...... 1 
