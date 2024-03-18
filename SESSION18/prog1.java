d69297f569bf9372d0215b65eba478317988afaa
Avni Goel <avnigoel98@gmail.com>
2024-03-19 00:00:30 +0530

    working on linkedlist - palindrome

---
+++ SESSION18/prog1.java
@@ -0,0 +1,174 @@
+package session16_17;
+
+public class prog {
+
+    static Node head;
+    static int size;
+
+    prog (){
+        size = 0;
+    }
+
+    static class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+            size++;
+        }
+    }
+
+    public void addFirst(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+
+    }
+
+    public void addLast(String data){
+        
+    }
+
+    public void deleteLast(String data){
+        
+    }
+
+    public void deleteFirst(String data){
+        
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public void printlist(){
+        Node currNode = head;
+
+        while (currNode != null){
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("null");
+    }
+
+    public void printList1(Node node)
+    {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    public static Node reverse(Node head){
+        Node curr = head; 
+        Node prev = null;
+
+        while(curr != null){
+            Node temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+
+        }
+        return prev;
+    }
+
+    public  static Node reverse1 (Node head){
+        if (head == null || head.next == null){
+            return head;
+        }
+
+        Node newHead = reverse1(head.next);
+        Node headNext = head.next;
+        headNext.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+
+
+
+
+    public static Node findMiddle(Node head){
+        Node slow = head;
+        Node fast = head;
+        while (fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static boolean isPalindrome(Node head){
+        if (head == null || head.next == null){
+            return true;
+        }
+
+        Node middle = findMiddle(head);
+        Node secondHalf = reverse(middle.next);
+        middle.next = null; // break the list into two halves
+
+        Node p1 = head;
+        Node p2 = secondHalf;
+        while(p1 != null && p2!= null){
+            if(p1.data != p2.data){
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        middle.next = reverse(secondHalf);
+        return true;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        prog  list = new prog();
+
+        // list.addFirst("a");
+        // list.addFirst("b");
+        // list.addFirst("c");
+        // list.addFirst("d");
+
+        list.head = new Node("1");
+        list.head.next = new Node ("2");
+        list.head.next.next = new Node ("3");
+
+        //list.printlist();
+
+        // a -> null
+        // b -> a -> null
+        // c -> b -> a -> null
+        // d -> c -> b -> a-> null
+
+        // reverse a linkedlist
+        //      head
+        //      1 -> 2 -> 3 -> 4 -> null
+        //prev curr temp
+        System.out.println("Original list: ");
+        list.printList1(head);
+        //head = list.reverse(head);
+        head = list.reverse1(head);
+        System.out.println("\n");
+
+        System.out.println("Reversed list: ");
+        System.out.println("\n");
+
+        list.printList1(head);
+
+
+       
+
+    }
+
+}
